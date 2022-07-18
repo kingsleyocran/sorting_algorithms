115f0508832c38793dfa366a47310597eb67d8e0
kingsleyocran <ocrankingsley@gmail.com>
2022-07-18 22:29:21 +0000

    add adv task 100

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,36 +1,33 @@
 #include "sort.h"
 
 /**
-  * shell_sort - A function tat sorts an array usin shell algorithm.
-  * @array: The array to sort.
-  * @size: The length of the array.
-  * Return: Nothing.
-  */
+* shell_sort -  function sorts an array of integers using
+*               the shell sort algorithm with Knuth sequence
+* @array: the array of integers
+* @size: size of the array
+* Return: none
+*/
 void shell_sort(int *array, size_t size)
 {
-	unsigned int i = 0, j = 0, gap = 0;
-	int aux = 0;
+	size_t gap = 0, j, i;
+	int tmp;
 
-	if (array == NULL || size < 2)
+	if (size < 2)
 		return;
 
-	while (gap < size / 3)
-		gap = gap * 3 + 1;
+	while ((gap = gap * 3 + 1) < size)
+		;
+
+	gap = (gap - 1) / 3;
 
 	for (; gap > 0; gap = (gap - 1) / 3)
 	{
 		for (i = gap; i < size; i++)
 		{
-			aux = array[i];
-			for (j = i; j >= gap && array[j - gap] > aux;
-					j -= gap)
-			{
-				if (array[j] != array[j - gap])
-					array[j] = array[j - gap];
-			}
-			if (array[j] != aux)
-				array[j] = aux;
-
+			tmp = array[i];
+			for (j = i; j >= gap && tmp <= array[j - gap]; j -= gap)
+				array[j] = array[j - gap];
+			array[j] = tmp;
 		}
 		print_array(array, size);
 	}
