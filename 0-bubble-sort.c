f0522c597dddfd9de7cae4d46535d5ae1690591b
kingsleyocran <ocrankingsley@gmail.com>
2022-07-18 20:47:20 +0000

    add task 0

--- 0-bubble-sort.c
+++ 0-bubble-sort.c
@@ -1,27 +1,28 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
-  * bubble_sort - A function for bubble sorting.
-  * @array: The array to sort.
-  * @size: THe length of the aaray.
-  * Return: Nothing.
-  */
+ * bubble_sort - sorts an array in ascending order
+ * @array: array of ints to sort
+ * @size: size of the array
+ */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i = 0, j = 0;
-	int aux = 0;
+	unsigned int i, j;
+	int tmp;
 
-	if (array == NULL || size == 0)
+	if (size < 2)
 		return;
-	for (; i < size - 1; i++)
+
+	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				aux = array[j + 1];
-				array[j + 1] = array[j];
-				array[j] = aux;
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
 				print_array(array, size);
 			}
 		}
