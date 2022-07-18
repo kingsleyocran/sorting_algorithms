06a9268e3b41df889feb9a8e17d5e7043c10a30e
kingsleyocran <ocrankingsley@gmail.com>
2022-07-18 22:39:07 +0000

    add adv task 104

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,60 +1,65 @@
 #include "sort.h"
 
 /**
- * heap_sort - sorts an array following the Heap sort algorithm
- * @array: array of ints to sort
- * @size: size of the array to sort
+ * swap_root - A function that swap the root nodes.
+ * @array: The heap to sort.
+ * @root: The root of the heap.
+ * @hi: The higher index.
+ * @size: The size of the array.
+ * Return: Nothing
  */
-void heap_sort(int *array, size_t size)
+void swap_root(int *array, size_t root, size_t hi, size_t size)
 {
-	int i;
-	int tmp;
+	size_t lo = 0, mi = 0, tmp = 0;
+	int aux = 0;
 
-	if (size < 2)
-		return;
-
-	for (i = size / 2 - 1; i >= 0; i--)
-		heapify(array, size, (size_t)i, size);
-
-	for (i = size - 1; i >= 0; i--)
+	while ((lo = (2 * root + 1)) <= hi)
 	{
-		tmp = array[i];
-		array[i] = array[0];
-		array[0] = tmp;
-		if (i != 0)
-			print_array(array, size);
-		heapify(array, (size_t)i, 0, size);
+		tmp = root;
+		mi = lo + 1;
+		if (array[tmp] < array[lo])
+			tmp = lo;
+		if (mi <= hi && array[tmp] < array[mi])
+			tmp = mi;
+		if (tmp == root)
+			return;
+		aux = array[root];
+		array[root] = array[tmp];
+		array[tmp] = aux;
+		print_array(array, size);
+		root = tmp;
 	}
 }
 
 /**
- * heapify - turns an array in a heap tree
- * @array: array to turn into heap
- * @s: size of the subtree
- * @root: index of the subtree in the heap
- * @size: size of the whole array
+ * heap_sort - A function that sorts an array using heap algorithm.
+ * @array: An array to sort.
+ * @size: The size of the array.
+ * Return: Nothing.
  */
-void heapify(int *array, size_t s, size_t root, size_t size)
+void heap_sort(int *array, size_t size)
 {
-	size_t max, left, right;
-	int tmp;
+	size_t hi = 0, gap = 0;
+	int tmp = 0;
 
-	max = root;
-	left = (root * 2) + 1;
-	right = (root * 2) + 2;
+		if (array == NULL || size < 2)
+			return;
 
-	if (left < s && array[left] > array[max])
-		max = left;
+		for (gap = (size - 2) / 2; 1; gap--)
+		{
+			swap_root(array, gap, size - 1, size);
+			if (gap == 0)
+				break;
+		}
 
-	if (right < s && array[right] > array[max])
-		max = right;
-
-	if (max != root)
+		hi = size - 1;
+		while (hi > 0)
 	{
-		tmp = array[root];
-		array[root] = array[max];
-		array[max] = tmp;
+		tmp = array[hi];
+		array[hi] = array[0];
+		array[0] = tmp;
 		print_array(array, size);
-		heapify(array, s, max, size);
+		hi--;
+		swap_root(array, 0, hi, size);
 	}
 }
